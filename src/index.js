17c51c6871c7c01b10097f94966738d23f69e46c
Miyabi <kodamamiyabi@gmail.com>
2020-11-22 14:50:42 +0900

    Add rest time.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ProgressBar from './progress-bar';
 
-class ProgressTimer extends React.Component {
+function ProgressTimer(props) {
+  return (
+    <div>
+      <h2>{msToTime(props.remain)}</h2>
+      <div className='main-progress-bar'>
+        <ProgressBar complete={100 - props.remain / props.duration * 100}/>
+      </div>
+    </div>
+  );
+}
+
+class PomodoroTimer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isWorking: true,
       currentDate: new Date(),
       endDate: null,
-      remain: props.duration,
+      remain: props.workDuration,
       isTicking: false,
-    };
+    }
 
     this.toggleStartStop = this.toggleStartStop.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
@@ -29,14 +41,15 @@ class ProgressTimer extends React.Component {
     this.setState(state => ({
       isTicking: false,
       endDate: null,
-      remain: this.props.duration,
+      isWorking: !state.isWorking,
+      remain: !state.isWorking ? this.props.workDuration : this.props.restDuration,
     }));
   }
 
   componentDidMount() {
     this.timerID = setInterval(
       () => this.tick(),
-      1,
+      10,
     );
   }
 
@@ -45,19 +58,30 @@ class ProgressTimer extends React.Component {
   }
 
   tick() {
-    this.setState(state => ({
-      currentDate: new Date(),
-      remain: state.isTicking ? state.endDate - new Date() : state.remain,
-    }));
+    if (this.state.remain > 0) {
+      this.setState(state => ({
+        currentDate: new Date(),
+        remain: state.isTicking ? state.endDate - new Date() : state.remain,
+      }));
+    }
+    else {
+      // 一度停止してから再開
+      this.setState(state => ({
+        isTicking: false,
+        endDate: null,
+        isWorking: !state.isWorking,
+        remain: !state.isWorking ? this.props.workDuration : this.props.restDuration,
+      }));
+
+      this.toggleStartStop();
+    }
   }
 
-  render() {
+  render(props) {
     return (
       <div>
-        <h2>{msToTime(this.state.remain)}</h2>
-        <div className='main-progress-bar'>
-        <ProgressBar complete={100 - this.state.remain / this.props.duration * 100}/>
-        </div>
+        <ProgressTimer remain={this.state.remain}
+          duration={this.state.isWorking ? this.props.workDuration : this.props.restDuration} />
         <div className='center'>
           <button onClick={this.toggleStartStop}>
             {this.state.isTicking ? 'Stop' : 'Start'}
@@ -67,21 +91,6 @@ class ProgressTimer extends React.Component {
           </button>
         </div>
       </div>
-    );
-  }
-}
-
-class PomodoroTimer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isWorking: true,
-    }
-  }
-
-  render(props) {
-    return (
-      <ProgressTimer duration={25*60*1000}></ProgressTimer>
     );
   }
 }
@@ -102,7 +111,7 @@ function tick() {
   const element = (
     <div>
       <h1>Pomodoro Timer</h1>
-      <PomodoroTimer />
+      <PomodoroTimer workDuration={10*1000} restDuration={3*1000} />
     </div>
   );
   ReactDOM.render(element, document.getElementById('root'));
